b171c1be9ffa52f3a92bf2860dfc14fe6a77e529
Abishek Mahi <abishekmas0708@gmail.com>
2022-11-25 13:42:05 +0530

    bg changed size changed and more

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ daynight.onclick = function () {
         // logodown.src = "images/logowhite.png"
     }
 }
+
+// Day and Night
+var daynights = document.getElementById("daynights");
+daynights.onclick = function () {
+    document.body.classList.toggle("dark_mode");
+    if (document.body.classList.contains("dark_mode")) {
+        daynights.src = "images/sun.png"
+        logo.src = "images/logowhite.png"
+        // logodown.src = "images/logoclr.png"
+
+    } else {
+        daynights.src = "images/moon.png"
+        logo.src = "images/logoclr.png"
+        // logodown.src = "images/logowhite.png"
+    }
+}
